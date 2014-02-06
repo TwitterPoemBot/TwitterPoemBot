ae7c70e7f0ccc5392c21449de9ae5ede3fc6273f
Mike <the.wishmaster07@gmail.com>
2014-02-05 20:41:26 -0600

    initial commit

---
+++ src/main.py
@@ -0,0 +1,8 @@
+__author__ = 'Mike'
+from twython import Twython
+
+
+if __name__ == '__main__':
+    twitter = Twython(app_key="7JpoIKJbcWppGabeAuyGA", app_secret="cVQGxy1fcxJJxJ3avyitZ4wNqAUEWNTIEgjNUDZnA",
+                  oauth_token="2329651538-iZ2nEPBSIyl3u5AnU4ppfYEfJflTEeH6Krl8OO5", oauth_token_secret="V6ja2kfgl3aNr28QvOb9VmlbP8e9jxkora82wplPT43Vz")
+    twitter.update_status(status='Test')

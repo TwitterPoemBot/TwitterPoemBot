a85f80ad61b2324685eb91056965e8a9709f03fe
Mettas <patelsvishal@gmail.com>
2014-03-07 15:44:27 -0800

    refactored out classes

--- src/rhymer/rhymer_test.py
+++ src/rhymer/rhymer_test.py
@@ -1,37 +1,34 @@
 #test cases for rhyme checker
 import unittest
-from rhyme_checker import Rhymer
+from rhyme_checker import rhymes, isInDict
 from cStringIO import StringIO
 
 class TestRhymerFunctions(unittest.TestCase):
 
-    def setUp(self):
-        self.rhymer = Rhymer()
-
     def test_rhyme(self):
         word1 = "call"
         word2 = "ball"
-        output = self.rhymer.rhymes(word1, word2)
+        output = rhymes(word1, word2)
         self.assertEqual(output, True)
 
     def test_no_rhyme(self):
         word1 = "call"
         word2 = "one"
-        output = self.rhymer.rhymes(word1, word2)
+        output = rhymes(word1, word2)
         self.assertEqual(output, False)
 
     def test_wrong_stress(self):
         word1 = "someone"
         word2 = "one"
-        output = self.rhymer.rhymes(word1, word2)
+        output = rhymes(word1, word2)
         self.assertEqual(output, False)
 
     def test_bad_input(self):
         word1 = "sq32e"
         word2 = "one"
-        output = self.rhymer.isInDict(word1)
+        output = isInDict(word1)
         self.assertEqual(output, False)
-        output = self.rhymer.rhymes(word1, word2)
+        output = rhymes(word1, word2)
         self.assertEqual(output, False)
 
 if __name__ == '__main__':

447a9cde2f2a153df78e450db0bd6d6ae242dac4
Mettas <patelsvishal@gmail.com>
2014-03-11 18:53:03 -0500

    changed name of site.py to fix some issues.

--- src/main.py
+++ src/main.py
@@ -1,12 +1,16 @@
 __author__ = 'Mike'
 from twython import Twython
-from site.models.poems.haiku import haiku
-from site.models.poems.parseLines import parse
+from sites.models.poems.haiku import haiku
+from sites.models.poems.parseLines import parse
+from sites.models.tweets.get_tweets2 import get_tweets_from_hashtag
 
 
 if __name__ == '__main__':
     twitter = Twython(app_key="7JpoIKJbcWppGabeAuyGA", app_secret="cVQGxy1fcxJJxJ3avyitZ4wNqAUEWNTIEgjNUDZnA",
                   oauth_token="2329651538-iZ2nEPBSIyl3u5AnU4ppfYEfJflTEeH6Krl8OO5", oauth_token_secret="V6ja2kfgl3aNr28QvOb9VmlbP8e9jxkora82wplPT43Vz")
+
+    results = get_tweets_from_hashtag(twitter, 'basketball')
+    print results[0]
 
     line1 = "five syllable line"
     line2 = "a seven syllable line"

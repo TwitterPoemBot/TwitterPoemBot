36bf181264a5c028ad5617326b3da47d43de8337
Mettas <patelsvishal@gmail.com>
2014-04-08 18:23:42 -0500

    fixed bug on twitter.py

--- src/twitterpoet/twitter.py
+++ src/twitterpoet/twitter.py
@@ -1,3 +1,5 @@
+from twython import Twython
+
 def getTwitter():
 	twitter = Twython(app_key="7JpoIKJbcWppGabeAuyGA", app_secret="cVQGxy1fcxJJxJ3avyitZ4wNqAUEWNTIEgjNUDZnA",oauth_token="2329651538-iZ2nEPBSIyl3u5AnU4ppfYEfJflTEeH6Krl8OO5", oauth_token_secret="V6ja2kfgl3aNr28QvOb9VmlbP8e9jxkora82wplPT43Vz")
 	return twitter

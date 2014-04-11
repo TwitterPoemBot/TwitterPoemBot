94759224c62b3ee8d3a707ac6f7467f100f3e403
Yuki <yukitokuhiro@icloud.com>
2014-04-11 17:16:13 -0500

    Adding parameterized testing for parseLines.

---
+++ src/twitterpoet/test/test_parse_parameterized.py
@@ -0,0 +1,26 @@
+#test cases for rhyme checker
+import unittest
+from cStringIO import StringIO
+import sys, logging
+
+sys.path.insert(0, '../') # temporary hack
+from models.poems.parseLines import parse, parse_all, is_english
+
+class TestParseFunctions(unittest.TestCase):
+    def test_invalid(self):
+        while True:
+            line = raw_input(r"Enter invalid line (or 'quit' to quit): ")
+            if line == 'quit':
+                return
+            self.assertEqual(parse(line), {})
+
+    def test_valid(self):
+        while True:
+            line = raw_input(r"Enter valid line (or 'quit' to quit): ")
+            if line == 'quit':
+                return
+            self.assertNotEqual(parse(line), {})
+
+if __name__ == '__main__':
+    logging.basicConfig(stream=sys.stderr, level=logging.INFO)
+    unittest.main()

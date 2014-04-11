d89db241b6cb40f5107e73e15fdac03427b7f054
Paul Osunero <esiex3@gmail.com>
2014-04-11 14:16:06 -0500

    fixing the docstrings

--- src/twitterpoet/models/poems/poem.py
+++ src/twitterpoet/models/poems/poem.py
@@ -5,19 +5,23 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI']= 'mysql://root:password@localhost/twitterpoem'
 db = SQLAlchemy(app)
 
-"""This is a model to allow poems to be saved in the db"""
 class Poem(db.Model):
+	"""This is a model to allow poems to be saved in the db"""
     id = db.Column(db.Integer, primary_key=True)
     poemText = db.Column(db.String(300))
 
-    """construtor for Poem"""
     def __init__(self, poemText):
+		"""construtor for Poem
+		
+		poemText: should be a string that represents the poem's text
+		"""
         self.poemText = poemText
 
     def __repr__(self):
         return '<Poem %r>' % self.poemText
 
-    """Saves a poem to the db"""
     def save(self):
+		"""Saves a poem to the db"""
         db.session.add(self)
         db.session.commit()
+        return self.id

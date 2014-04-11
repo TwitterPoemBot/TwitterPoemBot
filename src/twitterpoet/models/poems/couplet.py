b49bed389f9723555a6458ef2dc1bbce89261f5b
Matthew Ford <ford16@illinois.edu>
2014-04-11 01:13:11 -0500

    couplet lines must be within 1 syllable of each other

--- src/twitterpoet/models/poems/couplet.py
+++ src/twitterpoet/models/poems/couplet.py
@@ -1,12 +1,11 @@
 from random import sample
-
 def couplet(corpus):
     ''' Generates a Couplet poem given a list of (text, syllable count, rhyme) tuples ''' 
 
-    def get_lines(phone, word):
-        return [line for line in corpus if line != {} and line['phone'] == phone and word.lower() != line['last_word'].lower()]
+    def get_lines(phone, word, syllables):
+        return [line for line in corpus if line != {} and line['phone'] == phone and word.lower() != line['last_word'].lower() and abs(line['syllables'] - syllables)<=1]
     def get_lines2():
-	return [line for line in corpus if line != {} and line['phone'] != None and len(get_lines(line['phone'], line['last_word']))>0]  
+	return [line for line in corpus if line != {} and line['phone'] != None and len(get_lines(line['phone'], line['last_word'], line['syllables']))>0]  
     try:
         # Get random sample from 5 or 7 syllable tweets
         first = sample(get_lines2(), 1)
@@ -14,9 +13,10 @@ def couplet(corpus):
 	phone = phone['phone']
 	second = first
 	word = first[0]['last_word']
-        if len(get_lines(phone, word)) > 0:
+	syl = first[0]['syllables']
+        if len(get_lines(phone, word, syl)) > 0:
 	    while word==second[0]['last_word']:
-                second = sample(get_lines(phone, word), 1)
+                second = sample(get_lines(phone, word, syl), 1)
 	else:
 	    raise Exception('Could not construct couplet - not enough tweets found')
     except ValueError as e:
